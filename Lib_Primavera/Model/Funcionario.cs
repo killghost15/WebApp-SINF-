698b67fede04147f444bf1f6f882b08996ffa403
Ricardo Magalhães <ricardo_magalhaes45@hotmail.com>
2016-12-12 01:23:50 +0000

    fix last commit

--- Lib_Primavera/Model/Funcionario.cs
+++ Lib_Primavera/Model/Funcionario.cs
@@ -5,29 +5,12 @@ using System.Web;
 
 namespace FirstREST.Lib_Primavera.Model
 {
-    public class Cliente
+    public class Funcionario
     {
-        public string Morada;
+        public string Nome { get; set; }
 
-        /* Exemplo para POST e GET com valores específicos
-         public string Morada
-        {
-            get
-            {
-                return "MORADA: " + _morada;
-            }
-            set
-            {
-                _morada = value;
-            }
-        }*/
+        public string Email { get; set; }
 
-        public string CodCliente { get; set; }
-
-        public string NomeCliente { get; set; }
-
-        public string NumContribuinte { get; set; }
-
-        public string Moeda { get; set; }
+        public string Password { get; set; }
     }
 }
