b0f6d46b781cb9f78691031660d13498478ae834
jp Mendonça <chrzzz@hotmail.com>
2016-12-12 23:11:41 +0000

    just not diving the lines acording to bin

--- Scripts para criar as tables extra/create pickingList.sql
+++ Scripts para criar as tables extra/create pickingList.sql
@@ -1,18 +1,24 @@
 USE [PRIDEMOSINF]
 GO
 
-/****** Object:  Table [dbo].[PickingList]    Script Date: 12-12-2016 16:48:41 ******/
+/****** Object:  Table [dbo].[PickingList]    Script Date: 12-12-2016 23:10:00 ******/
 SET ANSI_NULLS ON
 GO
 
 SET QUOTED_IDENTIFIER ON
 GO
 
+SET ANSI_PADDING ON
+GO
+
 CREATE TABLE [dbo].[PickingList](
 	[Id] [int] IDENTITY(1,1) NOT NULL,
+	[Artigo] [nvarchar](48) NOT NULL,
+	[Localizacao] [varchar](30) NOT NULL,
+	[Quantidade] [int] NOT NULL,
 	[EstadoTratado] [bit] NOT NULL,
+	[IdECL] [uniqueidentifier] NOT NULL,
 	[idLinha] [uniqueidentifier] NOT NULL,
-	[IdCabecDoc] [uniqueidentifier] NOT NULL,
  CONSTRAINT [PK_PickingList] PRIMARY KEY CLUSTERED 
 (
 	[Id] ASC
@@ -21,7 +27,24 @@ CREATE TABLE [dbo].[PickingList](
 
 GO
 
-ALTER TABLE [dbo].[PickingList]  WITH CHECK ADD  CONSTRAINT [FK_PickingList_CabecDoc] FOREIGN KEY([IdCabecDoc])
+SET ANSI_PADDING OFF
+GO
+
+ALTER TABLE [dbo].[PickingList]  WITH CHECK ADD  CONSTRAINT [FK_PickingList_ArmazemLocalizacoes] FOREIGN KEY([Localizacao])
+REFERENCES [dbo].[ArmazemLocalizacoes] ([Localizacao])
+GO
+
+ALTER TABLE [dbo].[PickingList] CHECK CONSTRAINT [FK_PickingList_ArmazemLocalizacoes]
+GO
+
+ALTER TABLE [dbo].[PickingList]  WITH CHECK ADD  CONSTRAINT [FK_PickingList_Artigo] FOREIGN KEY([Artigo])
+REFERENCES [dbo].[Artigo] ([Artigo])
+GO
+
+ALTER TABLE [dbo].[PickingList] CHECK CONSTRAINT [FK_PickingList_Artigo]
+GO
+
+ALTER TABLE [dbo].[PickingList]  WITH CHECK ADD  CONSTRAINT [FK_PickingList_CabecDoc] FOREIGN KEY([IdECL])
 REFERENCES [dbo].[CabecDoc] ([Id])
 GO
 
--- Scripts para criar as tables extra/create pickingWave.sql
+++ Scripts para criar as tables extra/create pickingWave.sql
@@ -1,36 +1,60 @@
 USE [PRIDEMOSINF]
 GO
 
-/****** Object:  Table [dbo].[PickingWave]    Script Date: 12-12-2016 16:50:18 ******/
+/****** Object:  Table [dbo].[PickingList]    Script Date: 12-12-2016 23:09:48 ******/
 SET ANSI_NULLS ON
 GO
 
 SET QUOTED_IDENTIFIER ON
 GO
 
-CREATE TABLE [dbo].[PickingWave](
+SET ANSI_PADDING ON
+GO
+
+CREATE TABLE [dbo].[PickingList](
 	[Id] [int] IDENTITY(1,1) NOT NULL,
-	[idPickingList] [int] NOT NULL,
- CONSTRAINT [PK_PickingWave] PRIMARY KEY CLUSTERED 
+	[Artigo] [nvarchar](48) NOT NULL,
+	[Localizacao] [varchar](30) NOT NULL,
+	[Quantidade] [int] NOT NULL,
+	[EstadoTratado] [bit] NOT NULL,
+	[IdECL] [uniqueidentifier] NOT NULL,
+	[idLinha] [uniqueidentifier] NOT NULL,
+ CONSTRAINT [PK_PickingList] PRIMARY KEY CLUSTERED 
 (
 	[Id] ASC
-)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY],
- CONSTRAINT [IX_PickingWave] UNIQUE NONCLUSTERED 
-(
-	[Id] ASC,
-	[idPickingList] ASC
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
 ) ON [PRIMARY]
 
 GO
 
-ALTER TABLE [dbo].[PickingWave]  WITH CHECK ADD  CONSTRAINT [FK_PickingWave_PickingList] FOREIGN KEY([idPickingList])
-REFERENCES [dbo].[PickingList] ([Id])
+SET ANSI_PADDING OFF
 GO
 
-ALTER TABLE [dbo].[PickingWave] CHECK CONSTRAINT [FK_PickingWave_PickingList]
+ALTER TABLE [dbo].[PickingList]  WITH CHECK ADD  CONSTRAINT [FK_PickingList_ArmazemLocalizacoes] FOREIGN KEY([Localizacao])
+REFERENCES [dbo].[ArmazemLocalizacoes] ([Localizacao])
 GO
 
-EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'(1,1)' , @level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'TABLE',@level1name=N'PickingWave', @level2type=N'CONSTRAINT',@level2name=N'PK_PickingWave'
+ALTER TABLE [dbo].[PickingList] CHECK CONSTRAINT [FK_PickingList_ArmazemLocalizacoes]
+GO
+
+ALTER TABLE [dbo].[PickingList]  WITH CHECK ADD  CONSTRAINT [FK_PickingList_Artigo] FOREIGN KEY([Artigo])
+REFERENCES [dbo].[Artigo] ([Artigo])
+GO
+
+ALTER TABLE [dbo].[PickingList] CHECK CONSTRAINT [FK_PickingList_Artigo]
+GO
+
+ALTER TABLE [dbo].[PickingList]  WITH CHECK ADD  CONSTRAINT [FK_PickingList_CabecDoc] FOREIGN KEY([IdECL])
+REFERENCES [dbo].[CabecDoc] ([Id])
+GO
+
+ALTER TABLE [dbo].[PickingList] CHECK CONSTRAINT [FK_PickingList_CabecDoc]
+GO
+
+ALTER TABLE [dbo].[PickingList]  WITH CHECK ADD  CONSTRAINT [FK_PickingList_LinhasDoc] FOREIGN KEY([idLinha])
+REFERENCES [dbo].[LinhasDoc] ([Id])
+GO
+
+ALTER TABLE [dbo].[PickingList] CHECK CONSTRAINT [FK_PickingList_LinhasDoc]
 GO
 
